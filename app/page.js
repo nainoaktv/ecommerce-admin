b9c6241805ac9aa335973aca919d9781f4878a73
nainoktv <nainoaktvillegas@gmail.com>
2023-10-09 23:52:46 -0700

    temporary, display user email if signed in

--- app/page.js
+++ app/page.js
@@ -1,30 +1,25 @@
 // import Image from "next/image";
 "use client";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
 
 export default function Home() {
-  const session = useSession();
-  console.log(session);
+  const { data: session } = useSession();
 
-  // if (session.status === "loading") {
-  //   return <p>Loading...</p>;
-  // } else if (session.status === "authenticated") {
-  //   return <p>User Authenticated</p>;
-  // } else if (session.status === "unauthenticated") {
-  //   return <p>User Unauthenticated</p>;
-  // }
-
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button
-          className="bg-white p-2 px-4 rounded-lg"
-          onClick={() => signIn("google")}
-        >
-          Login with Google
-        </button>
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            className="bg-white p-2 px-4 rounded-lg"
+            onClick={() => signIn("google")}
+          >
+            Login with Google
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <div>Logged In {session.user.email}</div>;
 }
