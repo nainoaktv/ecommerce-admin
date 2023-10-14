c88132c4764259db5fb918ca761d11223bdb4d86
nainoktv <nainoaktvillegas@gmail.com>
2023-10-13 23:11:52 -0700

    POST request to create new products

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,4 +1,10 @@
+import mongooseConnect from "@/lib/mongoose";
+import Product from "@/models/Product";
+import { NextResponse } from "next/server";
+
 export async function POST(request) {
-  const res = await request.json();
-  return Response.json({ res });
+  const { productName, description, price } = await request.json();
+  await mongooseConnect();
+  await Product.create({ productName, description, price });
+  return NextResponse.json({ message: "Product Added" }, { status: 201 });
 }
