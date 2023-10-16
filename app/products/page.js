11f4436ce4b8fbdb38412d7271beca940119ee13
nainoktv <nainoaktvillegas@gmail.com>
2023-10-16 02:20:14 -0700

    add table feature to admin products with placeholder styling

--- app/products/page.js
+++ app/products/page.js
@@ -1,7 +1,17 @@
+"use client";
 import Link from "next/link";
 import Layout from "../components/Layout";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
   return (
     <Layout>
       <div className="max-w-max">
@@ -26,6 +36,22 @@ export default function Products() {
           Add New Products
         </Link>
       </div>
+      <table className="basic">
+        <thead>
+          <tr>
+            <td>Product Name</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((p) => (
+            <tr key={p.id}>
+              <td>{p.productName}</td>
+              <td>buttons</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Layout>
   );
 }
