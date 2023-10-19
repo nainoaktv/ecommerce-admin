8f09d83c36fb910818a8d8cabbf828292154bcea
nainoktv <nainoaktvillegas@gmail.com>
2023-10-19 01:25:20 -0700

    delete product request working, tested with postman

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -13,3 +13,10 @@ export async function GET() {
   await mongooseConnect();
   return NextResponse.json(await Product.find());
 }
+
+export async function DELETE(request) {
+  const id = request.nextUrl.searchParams.get("id");
+  await mongooseConnect();
+  await Product.findByIdAndDelete(id);
+  return NextResponse.json({ message: "Product deleted" }, { status: 200 });
+}
